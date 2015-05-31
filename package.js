c0844a4fc9ec8d95d39f0e78774a886bd4efde8f
Arve Knudsen <arve.knudsen@gmail.com>
2015-05-31 13:08:25 +0200

    Add Meteor package definition

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  summary: 'DropzoneJS.',
+  version: '4.0.0',
+  git: 'https://github.com/enyo/dropzone',
+  name: 'aknudsen:dropzone'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@1.1.0.2');
+  api.use('coffeescript');
+  api.use('fourseven:scss');
+  api.addFiles([
+      'src/basic.scss',
+      'src/dropzone.scss',
+      'src/dropzone.coffee',
+    ], 'client');
+  api.export('Dropzone', 'client');
+});
